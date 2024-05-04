727796e1ea42f81d4f6661c7d129eaadd9882836
Olawoyin Samson <olawoyin.samson@outlook.com>
2024-05-04 15:13:06 +0100

    complete new investment registration page
    Signed-off-by: Olawoyin Samson <olawoyin.samson@outlook.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
 
 
+const newInvestBtn = document.getElementById("registration-btn");
+
+const registrationBox = document.querySelector("#new_investment_box");
+
+const newInvestmentClose = document.getElementById("btn-cancel");
+
+const newInvestmentClose2 = document.getElementById("btn-cancel2");
+
+const overlay = document.getElementById("overlay");
+
+// Handle new investment modal box
+newInvestBtn.addEventListener("click", (e) => {
+  registrationBox.classList.remove("off-display");
+
+  // Call the overlay background for the modal display
+  overlay.style.display = "block";
+  //registrationBox.className.remove("off-display");
+})
+
+//Close new investment modal box
+newInvestmentClose.addEventListener("click", handleCloseModal);
+
+newInvestmentClose2.addEventListener("click", handleCloseModal);
+
+function handleCloseModal(){
+    registrationBox.classList.add("off-display");
+  
+    //close the page overlay after modal been closed
+    overlay.style.display = "none";
+  }  
+
